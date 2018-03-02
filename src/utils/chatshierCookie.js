4063417431d229c13d66bb173d8146c4329c8f45
PMH <9thflr.minghepan@gmail.com>
2018-03-02 13:21:14 +0800

    add(utils): add chatshierCookie.js

---
+++ src/utils/chatshierCookie.js
@@ -0,0 +1,84 @@
+const CHSR_COOKIE = {
+    USER_EMAIL: '_chsr_email',
+    USER_NAME: '_chsr_username'
+};
+const INIT_TIME = 'Thu, 01 Jan 1970 00:00:00 UTC';
+
+class ChatshierCookie {
+    constructor() {
+        this.domain = window.location.host.replace(/:\d+$/i, '');
+        this.DEFAULT_DOMAIN = this.domain.replace(/^[\w-]+\./i, '.');
+
+        this.minTimeGap = 60 * 1000;
+        this.hourTimeGap = 60 * this.minTimeGap;
+        this.dayTimeGap = 24 * this.hourTimeGap;
+        this.yearTimeGap = 365 * this.dayTimeGap;
+
+        // 刪除之前使用的 cookie 數值
+        this.getCookie('name') && this.deleteCookie('name');
+        this.getCookie('email') && this.deleteCookie('email');
+    }
+
+    /**
+     * @param {string} name
+     * @param {string} val
+     * @param {string} expires
+     * @param {string} [domain]
+     * @returns {boolean}
+     */
+    setCookie(name, val, expires, domain) {
+        if (!name) {
+            return false;
+        }
+
+        val = val || '';
+        expires = expires || new Date(Date.now() + this.yearTimeGap).toUTCString();
+        domain = domain || this.DEFAULT_DOMAIN;
+
+        document.cookie = name + '=' + val + ';expires=' + expires + ';domain=' + domain;
+        return true;
+    }
+
+    /**
+     * @param {string} name
+     */
+    getCookie(name) {
+        let cookieValues = '; ' + document.cookie;
+        let parts = cookieValues.split('; ' + name + '=');
+
+        if (parts.length >= 2) {
+            return unescape(parts.pop().split(';').shift());
+        }
+        return '';
+    }
+
+    /**
+     * @param {string} name
+     * @returns {boolean}
+     */
+    deleteCookie(name) {
+        let hasCookie = !!this.getCookie(name);
+        if (hasCookie) {
+            this.setCookie(name, '', INIT_TIME);
+            hasCookie = !!this.getCookie(name);
+
+            // 確保 cookie 有被清除
+            if (hasCookie) {
+                // 如果沒有被清除試著將 domain 的層級往上提升一層
+                let domainSplits = this.DEFAULT_DOMAIN.split('.');
+                domainSplits.shift();
+
+                while (hasCookie && domainSplits.length >= 2) {
+                    domainSplits.shift();
+                    let domain = '.' + domainSplits.join('.');
+                    this.setCookie(name, '', INIT_TIME, domain);
+                    hasCookie = !!this.getCookie(name);
+                }
+            }
+        }
+        return true;
+    }
+}
+
+export default new ChatshierCookie();
+export { CHSR_COOKIE };

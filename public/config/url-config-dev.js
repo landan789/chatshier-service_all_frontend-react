179dced8b1de5cb71dedf11073b6ce119a2f4b72
PMH <9thflr.minghepan@gmail.com>
2018-03-05 09:50:31 +0800

    remove unused files

--- public/config/url-config-dev.js
+++
@@ -1,8 +0,0 @@
-window.urlConfig = {
-    wwwUrl: '',
-    index: '/index.html',
-    terms: '/terms.html',
-    privacy: '/privacy.html',
-    webhookUrl: 'https://dev-chatshier.herokuapp.com/webhook',
-    apiUrl: 'https://api.chatshier.com/'
-};

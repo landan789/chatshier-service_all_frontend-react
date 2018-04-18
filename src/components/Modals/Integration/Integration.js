c53e4e8ed0f1f0591c37ba377f7b34c0d88485b5
TED <hwanghsin@9thflr.com>
2018-04-18 15:45:14 +0800

    Google API驗證完成

--- src/components/Modals/Integration/Integration.js
+++ src/components/Modals/Integration/Integration.js
@@ -3,21 +3,86 @@ import PropTypes from 'prop-types';
 import { Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input } from 'reactstrap';
 import Switch from 'react-switch';
 
+import { CLIENT_ID, SCOPES } from '../../../config/google-app';
+
 import './Integration.css';
 
 class Integration extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            google: false
+            google: false,
+            googleAuth: null
         };
         this.turnOnGoogleAPI = this.turnOnGoogleAPI.bind(this);
+        this.loadCalendarApi = this.loadCalendarApi.bind(this);
     }
 
     componentDidMount() {
+        this.loadCalendarApi();
+    }
+
+    loadCalendarApi() {
+        const script = document.createElement('script');
+        script.src = 'https://apis.google.com/js/api.js';
+        document.head.appendChild(script);
+
+        script.onload = () => {
+            return new Promise((resolve, reject) => {
+                window.gapi.load('client:auth2', () => {
+                    resolve();
+                });
+            }).then(() => {
+                return window.gapi.client.init({
+                    clientId: CLIENT_ID,
+                    scope: SCOPES
+                });
+            }).then(() => {
+                return window.gapi.auth2.getAuthInstance();
+            }).then((auth) => {
+                this.setState({
+                    googleAuth: auth,
+                    google: auth.isSignedIn.get()
+                });
+            });
+        };
+    }
+
+    signIn() {
+        if (!this.state.googleAuth) {
+            return Promise.reject(new Error('no_google_auth_instance'));
+        }
+
+        let signInOpts = {
+            prompt: 'select_account' // 每一次進行登入時都 popup 出選擇帳號的視窗
+        };
+
+        return this.state.googleAuth.signIn(signInOpts).then(function(currentUser) {
+            if (!currentUser) {
+                return;
+            }
+            return currentUser.getBasicProfile();
+        });
+    }
+
+    signOut() {
+        return this.state.googleAuth.signOut();
     }
 
     turnOnGoogleAPI() {
+        if (this.state.google) {
+            this.signOut().then(() => {
+                this.setState({google: false});
+            });
+            return;
+        }
+        this.signIn().then(() => {
+            this.setState({google: true});
+        }).catch(() => {
+            if (this.state.gooele) {
+                this.setState({google: false});
+            }
+        });
     }
 
     render() {
---
+++ src/config/google-app.js
@@ -0,0 +1,6 @@
+module.exports = {
+    API_KEY: '',
+    CLIENT_ID: '710049392797-41g539dngajh9oknu8rbagns1ck0c1n7.apps.googleusercontent.com',
+    DISCOVERY_DOCS: [],
+    SCOPES: 'https://www.googleapis.com/auth/calendar'
+};
